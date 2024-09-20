b6033e736e154f588529f2c5c69c9312f5dcbe4f
Juan Alberto Sanchez Balches <juansanbalches@gmail.com>
2024-09-20 14:19:32 +0200

    Add stage task for Heroku deployment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -41,3 +40,9 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
+
+// Añade esta sección al final
+tasks.register("stage") {
+    dependsOn("build")
+}
+
