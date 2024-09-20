8fca607d71dce35a20ae00f14604535c1cd50e8e
Juan Alberto Sanchez Balches <juansanbalches@gmail.com>
2024-09-20 12:28:05 +0200

    Menu principal hecho, con los 3 botones de las activity's activityEmocion creado (antiguo mainActivity)
    Listo para hacer: o reconocimiento de emociones ya, o preparar las demas activity's (agenda y explorar)

---
+++ app/src/main/java/com/testeando/botonreconoceraudio/TODOList.java
@@ -0,0 +1,16 @@
+package com.testeando.botonreconoceraudio;
+
+/*
+TODO List:
+- Convertir el MainActivity en un MENU que tenga botones:
+    Emocion (activity boton escuchar emociones) //HECHO
+    Agenda (activity que muestre toda tu agenda, para que puedas ver a quienes tienes agregado)
+    Explorar (activity que muestre los dispositivos encontrados por bluetooth en una lista, y al darle puedas agregarlo a tu agenda)
+- Mejorar la interfaz de usuario, hacerla más bonita.
+
+-BOTON EMOCION ACTIVITY será lo mismo que mainactivity ahora, quitando lo de poner tu nombre si es nuevo ingreso, que se lo debo de dejar al menu nuevo de mainActivity
+
+
+*/
+public class TODOList {
+}
